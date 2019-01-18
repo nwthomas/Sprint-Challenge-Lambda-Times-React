4f5d68c51bbd7eede8771eaec7ca8a16d54b142e
Nathan Thomas <nwthomas@me.com>
2019-01-18 10:04:46 -0800

    Update Cards/Card components with Styled Components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,9 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const CardClass = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+
+  div:first-child {
+    font-size: 25px;
+    font-family: Didot, serif;
+  }
+
+  div:last-child {
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+    margin-top: 15px;
+
+    div {
+      padding-right: 10px;
+      border-right: 1px solid lightgrey;
+      height: 40px;
+
+      img {
+        width: 40px;
+      }
+    }
+
+    span {
+      padding-left: 10px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;
+    }
+  }
+`;
 
 const Card = props => {
   return (
-    <div className="card">
+    <CardClass>
       <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
@@ -11,7 +54,7 @@ const Card = props => {
         </div>
         <span>By {props.card.author}</span>
       </div>
-    </div>
+    </CardClass>
   );
 };
 
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Card from "./Card";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsContainer>
       {props.cards.map((card, index) => {
         return <Card card={card} key={`key ${index}`} />;
       })
       /* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/
       }
-    </div>
+    </CardsContainer>
   );
 };
 
